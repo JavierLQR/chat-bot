1ce80521754799bd8074d75a505d13b91e7ad3da
rodrigo15975 <javierluis15975@hotmail.com>
2025-08-13 04:19:38 -0500

    feat: implement Pusher service integration with authentication endpoint

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@nestjs/common'
+import { PusherService } from '../pusher/pusher.service'
 
 @Injectable()
 export class AuthService {
   constructor(private readonly pusherService: PusherService) {}
+
+  /**
+   * autenticacion
+   */
+  public async autentication() {}
 }
--- src/modules/pusher/pusher.service.ts
+++ src/modules/pusher/pusher.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import * as Pusher from 'pusher'
 
 @Injectable()
 export class PusherService {
+  private readonly pusher: Pusher
+
+  constructor(private readonly configService: ConfigService) {
+    this.pusher = new Pusher({
+      appId: this.configService.getOrThrow<string>('PUSHER_APP_ID'),
+      key: this.configService.getOrThrow<string>('PUSHER_KEY'),
+      secret: this.configService.getOrThrow<string>('PUSHER_SECRET'),
+      cluster: this.configService.getOrThrow<string>('PUSHER_CLUSTER'),
+      useTLS: true,
+    })
+  }
+
   /**
    * autenticacion
    */
