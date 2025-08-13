c2393fca8b8425ffd71202f2bfed0835e6a6a630
rodrigo15975 <javierluis15975@hotmail.com>
2025-08-13 04:15:57 -0500

    update pusher.service.ts

--- src/modules/pusher/pusher.service.ts
+++ src/modules/pusher/pusher.service.ts
@@ -1,26 +1,9 @@
 import { Injectable } from '@nestjs/common'
-import { CreatePusherDto } from './dto/create-pusher.dto'
-import { UpdatePusherDto } from './dto/update-pusher.dto'
 
 @Injectable()
 export class PusherService {
-  create(createPusherDto: CreatePusherDto) {
-    return 'This action adds a new pusher'
-  }
-
-  findAll() {
-    return `This action returns all pusher`
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} pusher`
-  }
-
-  update(id: number, updatePusherDto: UpdatePusherDto) {
-    return `This action updates a #${id} pusher`
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} pusher`
-  }
+  /**
+   * autenticacion
+   */
+  public async autentication() {}
 }
