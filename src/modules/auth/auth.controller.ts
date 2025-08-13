b3f7df80959d86a24ec025a30b9ae134fbf9457c
rodrigo15975 <javierluis15975@hotmail.com>
2025-08-13 04:15:43 -0500

    update auth.controller.ts

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -7,9 +7,4 @@ import { AuthService } from './auth.service'
 })
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
-  @Post('autentication')
-  autentication() {
-    return this.authService.autentication()
-  }
 }
