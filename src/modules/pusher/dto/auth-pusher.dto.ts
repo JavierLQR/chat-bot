969a5721126797a5b29854c51866ed6d33195d50
rodrigo15975 <javierluis15975@hotmail.com>
2025-08-13 21:31:05 -0500

    feat: add AuthPusherDto and remove unused Pusher DTOs

---
+++ src/modules/pusher/dto/auth-pusher.dto.ts
@@ -0,0 +1,4 @@
+export class AuthPusherDto {
+  socketId: string
+  channel: string
+}
--- src/modules/pusher/dto/update-pusher.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types'
-import { CreatePusherDto } from './create-pusher.dto'
-
-export class UpdatePusherDto extends PartialType(CreatePusherDto) {}
