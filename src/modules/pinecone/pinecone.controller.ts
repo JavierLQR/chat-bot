de66085c7b078a2b58dc85269682622182f13c6b
rodrigo15975 <javierluis15975@hotmail.com>
2025-08-20 19:30:21 -0500

    feat: add POST endpoint for creating Pinecone index

--- src/modules/pinecone/pinecone.controller.ts
+++ src/modules/pinecone/pinecone.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
 import { PineconeService } from './pinecone.service'
+import { CreatePineconeIndexDto } from './dto/create-pinecone.dto'
 
-@Controller('pinecone')
+@Controller({
+  path: 'pinecone',
+})
 export class PineconeController {
   constructor(private readonly pineconeService: PineconeService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  createIndex(@Body() createPineconeIndexDto: CreatePineconeIndexDto) {
+    console.log({
+      createPineconeIndexDto,
+    })
+
+    return this.pineconeService.createIndex(createPineconeIndexDto)
+  }
 }
