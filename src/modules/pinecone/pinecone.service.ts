27eb834d312d792dc5ab3c16dd703b551b6e0afa
rodrigo15975 <javierluis15975@hotmail.com>
2025-08-20 19:30:27 -0500

    feat(pinecone): implement Pinecone service with index creation functionality

--- src/modules/pinecone/pinecone.service.ts
+++ src/modules/pinecone/pinecone.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common'
+import {
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { Pinecone } from '@pinecone-database/pinecone'
+import ApiResponse from 'src/common/utils/response.client'
+import { CreatePineconeIndexDto } from './dto/create-pinecone.dto'
 
 @Injectable()
-export class PineconeService {}
+export class PineconeService {
+  private readonly pinecone: Pinecone
+  private readonly logger: Logger = new Logger(PineconeService.name)
+
+  constructor(private readonly configService: ConfigService) {
+    this.pinecone = new Pinecone({
+      apiKey: this.configService.getOrThrow<string>('PINECONE_API_KEY'),
+      maxRetries: 3,
+    })
+  }
+
+  /**
+   * createIndex
+   */
+  public async createIndex(createPineconeIndexDto: CreatePineconeIndexDto) {
+    const {
+      name,
+      deletionProtection,
+      dimension,
+      metric,
+      spec,
+      tags,
+      vectorType,
+    } = createPineconeIndexDto
+    this.logger.verbose('Creating Pinecone index...')
+    try {
+      await this.pinecone.createIndex({
+        name,
+        vectorType,
+        dimension,
+        metric,
+        spec,
+        deletionProtection,
+        tags,
+      })
+
+      this.logger.verbose('Pinecone index created successfully')
+
+      return ApiResponse({
+        message: 'Pinecone index created successfully',
+        service: PineconeService.name,
+        statusCode: HttpStatus.OK,
+      })
+    } catch (error) {
+      this.logger.error('Error creating Pinecone index:', error)
+      throw new InternalServerErrorException(
+        'Failed to create Pinecone index',
+        {
+          cause: error,
+          description: (error as Error).message,
+        },
+      )
+    }
+  }
+}
