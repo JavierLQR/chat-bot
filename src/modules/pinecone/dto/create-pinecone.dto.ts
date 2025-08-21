e415cfd02883987911df9859bb8bf85686cfbfc6
rodrigo15975 <javierluis15975@hotmail.com>
2025-08-20 19:30:35 -0500

    feat: implement Pinecone index creation DTO with validation

--- src/modules/pinecone/dto/create-pinecone.dto.ts
+++ src/modules/pinecone/dto/create-pinecone.dto.ts
@@ -1,1 +1,90 @@
-export class CreatePineconeDto {}
+import { Type } from 'class-transformer'
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  ValidateNested,
+} from 'class-validator'
+
+export enum VectorType {
+  DENSE = 'dense',
+  SPARSE = 'sparse',
+}
+
+export enum Metric {
+  COSINE = 'cosine',
+  EUCLIDEAN = 'euclidean',
+  DOTPRODUCT = 'dotproduct',
+}
+
+export enum DeletionProtection {
+  ENABLED = 'enabled',
+  DISABLED = 'disabled',
+}
+
+export enum TagsEnum {
+  DEVELOPMENT = 'development',
+  PRODUCTION = 'production',
+}
+
+export enum Dimension {
+  DEFAULT = 1024, // Default dimension for MistralAI embeddings
+  EMBEDDING = 1536, // Dimension for OpenAI text-embedding-3-small
+}
+
+class TagsDto {
+  [key: string]: string
+}
+
+class TypeSpec {
+  @IsEnum(['aws', 'gcp', 'azure'], {
+    message: 'cloud must be one of the following: aws, gcp, azure',
+  })
+  cloud: 'aws' | 'gcp' | 'azure'
+
+  @IsString()
+  @IsNotEmpty()
+  region: string
+}
+
+class Spec {
+  @ValidateNested()
+  @Type(() => TypeSpec)
+  serverless: TypeSpec
+}
+
+export class CreatePineconeIndexDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string
+
+  @IsEnum(VectorType)
+  vectorType: VectorType
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsEnum(Dimension, {
+    message: 'dimension must be one of the following: 1024, 1536',
+  })
+  dimension: Dimension
+
+  @IsEnum(Metric, {
+    message:
+      'metric must be one of the following: cosine, euclidean, dotproduct',
+  })
+  metric: Metric
+
+  @ValidateNested()
+  @Type(() => Spec)
+  spec: Spec
+
+  @IsEnum(DeletionProtection, {
+    message: 'deletionProtection must be either enabled or disabled',
+  })
+  deletionProtection: DeletionProtection
+
+  @ValidateNested()
+  @Type(() => TagsDto)
+  tags: Record<string, string>
+}
--- src/modules/pinecone/dto/update-pinecone.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types'
-import { CreatePineconeDto } from './create-pinecone.dto'
-
-export class UpdatePineconeDto extends PartialType(CreatePineconeDto) {}
