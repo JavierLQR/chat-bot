8209b1ca81b36554ec1907fcabed7e01f766be3c
rodrigo15975 <javierluis15975@hotmail.com>
2025-08-23 22:01:14 -0500

    Add ProductsModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { GlobalsModules } from './config/globals.module'
 import { PusherModule } from './modules/pusher/pusher.module'
 import { AuthModule } from './modules/auth/auth.module'
 import { PineconeModule } from './modules/pinecone/pinecone.module'
+import { ProductsModule } from './modules/products/products.module'
 
 @Module({
-  imports: [GlobalsModules, PusherModule, AuthModule, PineconeModule],
+  imports: [
+    GlobalsModules,
+    PusherModule,
+    AuthModule,
+    PineconeModule,
+    ProductsModule,
+  ],
 })
 export class AppModule {}
