311f2771e6628b77a44e294b2c63af5317fb409e
rodrigo15975 <javierluis15975@hotmail.com>
2025-08-29 20:17:59 -0500

    update products.service.ts

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from 'nestjs-prisma'
 import { PineconeService } from '../pinecone/pinecone.service'
+import { LanchaingMitralService } from '../lanchaing-mitral/lanchaing-mitral.service'
 
 @Injectable()
 export class ProductsService {
   constructor(
     private readonly prismaService: PrismaService,
     private readonly pineneconeService: PineconeService,
+    private readonly lanchaingMitralService: LanchaingMitralService,
   ) {}
 
   /**
    * insertDataProductsFoods
    */
-  public insertDataProductsFoods() {}
+  public async insertDataProductsFoods() {
+    await this.PineconeStore.fromExistingIndex(
+      this.lanchaingMitralService.MistralAIEmbeddings,
+      {
+        namespace: 'products-foods',
+        maxRetries: 3,
+        maxConcurrency: 5,
+      },
+    )
+  }
 
   public get PineconeStore() {
     return this.pineneconeService.PineconeStore
